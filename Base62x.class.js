91f5e767f9e3c2437dd28cb428e997241b4acc4d
Wadelau <wadelau@gmail.com>
2017-02-13 18:18:16 +0800

    updates on js ver.

--- Base62x.class.js
+++ Base62x.class.js
@@ -9,6 +9,7 @@
  * v0.8, 21:49 12 February 2017
  */
  'use strict';
+ //- Assume We Are in Charset of UTF-8 Runtime.
  
  class Base62x {
 	 
@@ -16,12 +17,12 @@
 	 constructor(){
 		 //- @todo, refer, https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes
 		 this.isdebug = true;
-		 this.i = Base62x.genRand(); // static method
+		 this.i = Math.ceil(Math.random()*100000); // Base62x.genRand(); // static method
 		 this.codetype = 0;
 	 }
 	 
 	 //- variables
-	 //var isdebug = true; // properties unsupported at present
+	 //var isdebug = true; // properties unsupported till now, 21:19 13 February 2017
 	 
 	 //- methods, public
 	 //- encode, statically
@@ -40,7 +41,7 @@
 		 var ascidx = this.get('ascidx');
 		 var ascmax = this.get('ascmax');
 		 var max_safe_base = this.get('max_safe_base');
-		 console.log('static encode: xtag:['+xtag+'] input:['+input+']');
+		 //console.log('static encode: xtag:['+xtag+'] input:['+input+']');
 		 var rb62x = this.fillRb62x(b62x, bpos, xpos);
 		 var isnum = false;
 		 if(ibase > 0){ $isnum = true; }
@@ -52,7 +53,7 @@
 		 }
 		 else{
 			 // string
-			 var inputArr = this.toUTF8Array(input); //input.split(''); // need '' as parameter
+			 var inputArr = this.toUTF8Array(input); // this.str2ab(input); //input.split(''); // need '' as parameter
 			 var inputlen = inputArr.length;
 			 var setResult = this.setAscii(codetype, inputArr, ascidx, ascmax, asclist, ascrlist);
 			 var asctype = setResult['asctype'];
@@ -63,7 +64,6 @@
 			 if(asctype == 1){ // ascii
 				 var ixtag = xtag.charCodeAt();
 				 do{
-					 //inputArr[i] = inputArr[i].charCodeAt();
 					 if(ascidx[inputArr[i]] != -1){ // why != here?
 						 op[m] = xtag;
 						 op[++m] = ascidx[inputArr[i]];
@@ -84,18 +84,16 @@
 				var c0=0; var c1=0; var c2=0; var c3=0; var remaini=0;
 				do{
 					remaini = inputlen - i;
-					//inputArr[i] = inputArr[i].charCodeAt();
 					switch(remaini){
 						case 1:
 							c0 = inputArr[i] >> 2;
-							c1 = ((inputArr[i] << 6) & 0xff >> 6);
+							c1 = ((inputArr[i] << 6) & 0xff) >> 6;
 							if(c0 > bpos){ op[m] = xtag; op[++m] = b62x[c0]; }
 							else{ op[m] = b62x[c0]; }
 							if(c1 > bpos){ op[++m] = xtag; op[++m] = b62x[c1]; }
 							else{ op[++m] = b62x[c1];}
 							break;
 						case 2:
-							//inputArr[i+1] = inputArr[i+1].charCodeAt();
 							c0 = inputArr[i] >> 2;
 							c1 = (((inputArr[i] << 6) & 0xff) >> 2) | (inputArr[i+1] >> 4);
 							c2 = ((inputArr[i+1] << 4) & 0xff) >> 4;
@@ -108,8 +106,6 @@
 							i += 1;
 							break;
 						default:
-							//inputArr[i+1] = inputArr[i+1].charCodeAt();
-							//inputArr[i+2] = inputArr[i+2].charCodeAt();
 							c0 = inputArr[i] >> 2;
 							c1 = (((inputArr[i] << 6) & 0xff) >> 2) | (inputArr[i+1] >> 4);
 							c2 = (((inputArr[i+1] << 4) & 0xff) >> 2) | (inputArr[i+2] >> 6);
@@ -128,7 +124,7 @@
 				}
 				while(++i < inputlen);
 			 }
-			 console.log('static enc: op:['+op+'] asctype:['+asctype+'] inputArr[0]:['+inputArr[0]+']');
+			 console.log('static enc: op:['+op+'] asctype:['+asctype+'] inputArr:['+inputArr+']');
 			 rtn = op.join('');
 		 }
 		 return rtn;
@@ -151,7 +147,7 @@
 		var ascidx = this.get('ascidx');
 		var ascmax = this.get('ascmax');
 		var max_safe_base = this.get('max_safe_base');
-		console.log('static decode: xtag:['+xtag+'] input:['+input+']');
+		//console.log('static decode: xtag:['+xtag+'] input:['+input+']');
 		var rb62x = this.fillRb62x(b62x, bpos, xpos);
 		var isnum = false;
 		if(obase > 0){ $isnum = true; }
@@ -164,7 +160,7 @@
 		}
 		else{
 			// string
-			var inputArr = this.toUTF8Array(input); //input.split(''); // need '' as parameter
+			var inputArr = this.toUTF8Array(input); // this.str2ab(input); //input.split(''); // need '' as parameter
 			var inputlen = inputArr.length;
 			var setResult = this.setAscii(codetype, inputArr, ascidx, ascmax, asclist, ascrlist);
 			var asctype = setResult['asctype'];
@@ -191,15 +187,20 @@
 					m++;
 				}
 				while(++i < inputlen);
+				rtn = op.join('');
 			}
 			else{ // non-ascii
 				var tmpArr = []; var tmprtn = {};
 				var bint = {1:1, 2:2, 3:3};
 				var remaini = 0;
 				var rki = 0;
-				for(var rk in rb62x){ // for char and its ascii value
+				for(var rk in rb62x){ // for char and its ascii value as key
 					rki = rk.charCodeAt();
 					rb62x[rki] = rb62x[rk];
+				}
+				for(var rk in bint){ // for char and its ascii value as key
+					rki = rk.charCodeAt();
+					bint[rki] = bint[rk];
 				}
 				do{
 					tmpArr = [];
@@ -231,26 +232,22 @@
 						default:
 							if(inputArr[i] == ixtag){ tmpArr[0] = bpos + bint[inputArr[++i]]; }
 							else{ tmpArr[0] = rb62x[inputArr[i]]; }
-							console.log('static decode: 0 i:['+i+'] tmpArr:['+tmpArr+'] 0:['+rb62x[inputArr[i]]+'] v:['+inputArr[i]+'] remaini:['+remaini+']');
 							if(inputArr[++i] == ixtag){ tmpArr[1] = bpos + bint[inputArr[++i]]; }
-							else{ tmpArr[1] = rb62x[inputArr[i]]; console.log('ddd i:['+i+']'); }
-							console.log('static decode: 1 i:['+i+'] tmpArr:['+tmpArr+'] 1:['+rb62x[inputArr[i]]+'] v:['+inputArr[i]+']');
+							else{ tmpArr[1] = rb62x[inputArr[i]]; }
 							if(inputArr[++i] == ixtag){ tmpArr[2] = bpos + bint[inputArr[++i]]; }
 							else{ tmpArr[2] = rb62x[inputArr[i]]; }
-							console.log('static decode: 2 i:['+i+'] tmpArr:['+tmpArr+']');
 							if(inputArr[++i] == ixtag){ tmpArr[3] = bpos + bint[inputArr[++i]]; }
 							else{ tmpArr[3] = rb62x[inputArr[i]]; }
 							tmprtn = this.decodeByLength(tmpArr, op, m);
 							op = tmprtn[0];
 							m = tmprtn[1];
-							console.log('static decode: i:['+i+'] tmpArr:['+tmpArr+'] tmprtn:['+JSON.stringify(tmprtn)+'] op:['+JSON.stringify(op)+'] m:['+m+'] ixtag:['+ixtag+']');
 					}
 					m++;
 				}
 				while(++i < inputlen);
+				//console.log('static dec: op:['+op+'] asctype:['+asctype+'] inputArr:['+inputArr+'] tmpstr:['+tmpstr+']');
+				rtn = this.toUTF16Array(op).join(''); //String.fromCharCode.apply(null, new Uint8Array(op));
 			}
-			console.log('static dec: op:['+op+'] asctype:['+asctype+'] inputArr:['+inputArr+'] rb62x:['+JSON.stringify(rb62x)+']');
-			rtn = op.join('');
 		}
 		return rtn;
 	 }
@@ -308,14 +305,14 @@
 		 //- runtime
 		 if(typeof this.configx != 'undefined'){
 			 if(this.configx.hasOwnProperty(k)){
-				ret = this.configx[k]; //eval('Base62x.configx.'+k);
+				ret = this.configx[k];
 				gotV = true;
 			 }
 		 }
 		 //- constant
 		 if(!gotV){
 			 if(this.config.hasOwnProperty(k)){
-				ret = this.config[k]; //eval('Base62x.config.'+k);
+				ret = this.config[k];
 				gotV = true;
 			 }
 		 }
@@ -331,11 +328,9 @@
 		 }
 		 if(true){
 			 if(typeof v == 'string'){
-				 //eval('Base62x.configx.'+k+'=\''+v+'\';');
 				 this.configx[k] = v;
 			 }
 			 else{
-				//eval('Base62x.configx.'+k+'='+v);
 				this.configx[k] = v;
 			 }
 		 }
@@ -353,7 +348,7 @@
 				 rb62x[b62x[i]] = i;
 			 }
 		 }
-		 console.log('static fillRb62x:['+JSON.stringify(rb62x)+'] b62x:['+b62x+'] bpos:['+bpos+'] xpos:['+xpos+']');
+		 //console.log('static fillRb62x:['+JSON.stringify(rb62x)+'] b62x:['+b62x+'] bpos:['+bpos+'] xpos:['+xpos+']');
 		 return rb62x;
 	 }
 	 
@@ -364,8 +359,6 @@
 		 var xtag = this.get('xtag');
 		 var ixtag = xtag.charCodeAt();
 		 var inputlen = inputArr.length;
-		 console.log('static setAscii: inputArr:['+JSON.stringify(inputArr)+'] len:['+inputlen+']');
-		 //if(codetype == 0 && inputArr[0].charCodeAt() <= ascmax){
 		 if(codetype == 0 && inputArr[0] <= ascmax){
 			 asctype = 1;
 			 var tmpi = 0;
@@ -382,9 +375,7 @@
 		 }
 		 else if(codetype == 1 && inputArr[inputlen-1] == ixtag){
 			 asctype = 1;
-			 console.log('static setAscii: codetype:['+codetype+'] ixtag:['+ixtag+']');
 		 }
-		 console.log('static setAscii: codetype:['+codetype+'] ixtag:['+ixtag+'] asctype:['+asctype+']');
 		 rtn['asctype'] = asctype;
 		 if(asctype == 1){
 			 for(var i=0; i<ascmax; i++){ ascidx[i] = -1; }
@@ -432,11 +423,6 @@
 	 }
 	 
 	 //-
-	 static genRand(){
-		 return Math.ceil(Math.random()*100000);
-	 }
-	 
-	 //-
 	 static decodeByLength(tmpArr, op, m){
 		 var rtn = {};
 		 var c0=0; var c1=0; var c2=0;
@@ -444,45 +430,45 @@
 			c0 = tmpArr[0] << 2 | tmpArr[1] >> 4;
 	        c1 = ((tmpArr[1] << 4) & 0xf0) | (tmpArr[2] >> 2);
 	        c2 = ((tmpArr[2] << 6) & 0xff) | tmpArr[3];
-	        op[m] = String.fromCharCode(c0);
-	        op[++m] = String.fromCharCode(c1);
-	        op[++m] = String.fromCharCode(c2);
+	        op[m] = c0;
+	        op[++m] = c1;
+	        op[++m] = c2;
 		 }
 		 else if(typeof tmpArr[2] != 'undefined'){
 			c0 = tmpArr[0] << 2 | tmpArr[1] >> 4;
 	        c1 = ((tmpArr[1] << 4) & 0xf0) | tmpArr[2];
-	        op[m] = String.fromCharCode(c0);
-	        op[++m] = String.fromCharCode(c1);
+	        op[m] = c0;
+	        op[++m] = c1;
 		 }
 		 else if(typeof tmpArr[1] != 'undefined'){
 			c0 = tmpArr[0] << 2 | tmpArr[1];
-	        op[m] = String.fromCharCode(c0);
+	        op[m] = c0;
 		 }
 		 else{
 			c0 = tmpArr[0];
-	        op[m] = String.fromCharCode(c0);
+	        op[m] = c0; //String.fromCharCode(c0);
 		 }
-		 console.log('static decodeByLength: tmpArr:['+tmpArr+'] op:['+op+'] m:['+m+']');
+		 //console.log('static decodeByLength: tmpArr:['+tmpArr+'] op:['+op+'] m:['+m+']');
 		 rtn = {0:op, 1:m};
 		 return rtn;
 	 }
 	 
 	 //-
-	 static toUTF8Array(str) { // http://stackoverflow.com/questions/18729405/how-to-convert-utf8-string-to-byte-array
-		var utf8 = [];
+	 static toUTF8Array(utf16Str) { // http://stackoverflow.com/questions/18729405/how-to-convert-utf8-string-to-byte-array
+		var utf8 = []; var str = utf16Str;
 		for (var i=0; i < str.length; i++) {
 			var charcode = str.charCodeAt(i);
-			if (charcode < 0x80){ utf8.push(charcode); }
-			else if (charcode < 0x800) {
+			if (charcode < 0x80){ utf8.push(charcode); } // one byte
+			else if (charcode < 0x800) { // two bytes
 				utf8.push(0xc0 | (charcode >> 6), 
 						  0x80 | (charcode & 0x3f));
 			}
-			else if (charcode < 0xd800 || charcode >= 0xe000) {
+			else if (charcode < 0xd800 || charcode >= 0xe000) { // three bytes
 				utf8.push(0xe0 | (charcode >> 12), 
 						  0x80 | ((charcode>>6) & 0x3f), 
 						  0x80 | (charcode & 0x3f));
 			}
-			else { // surrogate pair
+			else { // surrogate pair, four bytes
 				i++;
 				// UTF-16 encodes 0x10000-0x10FFFF by
 				// subtracting 0x10000 and splitting the
@@ -497,5 +483,45 @@
 		}
 		return utf8;
 	}
-	 
+	
+	//-
+	static toUTF16Array(utf8Bytes){ //https://terenceyim.wordpress.com/2011/03/04/javascript-utf-8-codec-that-supports-supplementary-code-points/
+		var bytes = utf8Bytes;
+		var len = bytes.length;
+		var result = [];
+		var code, i, j; j=0;
+		for(i = 0; i<len; i++){
+			if(bytes[i] <= 0x7f){ // one byte                    
+				result[j++] = String.fromCharCode(bytes[i]);
+			}
+			else if(bytes[i] >= 0xc0){  // Mutlibytes
+				if(bytes[i] < 0xe0){  // two bytes
+					code = ((bytes[i++] & 0x1f) << 6) |
+							(bytes[i] & 0x3f);
+				}
+				else if(bytes[i] < 0xf0){  // three bytes
+					code = ((bytes[i++] & 0x0f) << 12) |
+						   ((bytes[i++] & 0x3f) << 6)  |
+							(bytes[i] & 0x3f);
+				}
+				else{  // four bytes
+					// turned into two characters in JS as surrogate pair
+					code = (((bytes[i++] & 0x07) << 18) |
+							((bytes[i++] & 0x3f) << 12) |
+							((bytes[i++] & 0x3f) << 6) |                                  
+							 (bytes[i] & 0x3f)) - 0x10000;
+					// High surrogate
+					result[j++] = String.fromCharCode(((code & 0xffc00) >>> 10) + 0xd800);
+					// Low surrogate
+					code = (code & 0x3ff) + 0xdc00;
+				}
+				result[j++] = String.fromCharCode(code);
+			}
+			else{ // Otherwise it's an invalid UTF-8, skipped
+				console.log('static toUTF16Array: illegal utf8 found. 1702132109.');
+			}
+		}
+		return result;
+	}
+
  }
