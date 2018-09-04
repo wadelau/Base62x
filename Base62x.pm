a24023a9a23202039d527ef568866c5b2cb44184
Liuzhenxing <liuzhenxing@parbattech.com>
2018-09-04 18:08:07 +0800

    Base62x.pm first release.

--- Base62x.pm
+++ Base62x.pm
@@ -3,18 +3,19 @@
 =pod
 
 * -Base62x in -Perl
-* Wadelau@ufqi.com
+* Wadelau@{ufqi,gmail,hotmail}.com
 * Refers to 
 *    http://ieeexplore.ieee.org/xpl/freeabs_all.jsp?arnumber=6020065
 *    -GitHub-Wadelau , base62x.c
 *    https://github.com/wadelau/Base62x
 *    https://ufqi.com/dev/base62x/?_via=-naturedns
-* Tue Aug  9 21:18:14 CST 2016
+* since Tue Aug  9 21:18:14 CST 2016
 * bugfix, 13:39 13 September 2016
 * bugfix, Thu Sep 29 04:06:26 UTC 2016
 * imprvs on numeric conversion, Fri Oct  7 03:42:59 UTC 2016
 * bugifx by _decodeByLength, 20:40 28 November 2016
-* Base62x in Perl, Thu Aug 30 20:49:27 CST 2018
+* Base62x in Perl, init, Thu Aug 30 20:49:27 CST 2018
+* Base62x in Perl, refine, Tue Sep  4 19:56:01 CST 2018
 
 =cut
 
@@ -27,12 +28,32 @@ use vars qw(@ISA @EXPORT $VERSION);
 
 require Exporter;
 @ISA = qw(Exporter);
-@EXPORT = qw(encode_base62x decode_base62x);
-#use POSIX qw(strftime);
-#use Date::Parse qw(str2time);
-#my $thedate = POSIX::strftime("%Y-%m-%d", gmtime()); 
-#my $thedate_nodash = POSIX::strftime("%Y%m%d", gmtime()); 
+@EXPORT = qw(base62x_encode base62x_decode);
 my $VERSION = '1.0';
+my $UTF8 = 'UTF-8';
+my $LOGTAG = 'Base62x';
+
+use constant {
+    XTAG => 'x',
+    ENCD => '-enc',
+    DECD => '-dec',
+    DEBG => '-v',
+    CVTN => '-n',
+    };
+my ($i, $isdebug, $codetype) = (0, 0, 0);
+my ($bpos, $xpos, $ascmax, $max_safe_base) = (60, 64, 127, 36);
+    # 0-60 chars; b62x[64] = 'x'
+my @b62x = ('0','1','2','3','4','5','6','7','8','9',
+        'A','B','C','D','E','F','G','H','I','J','K','L','M','N',
+        'O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b',
+        'c','d','e','f','g','h','i','j','k','l','m','n','o','p',
+        'q','r','s','t','u','v','w','y','z','1','2','3','x');
+my @asclist = ('4','5','6','7','8','9', '0',
+        'A','B','C','D','E','F','G','H','I','J','K','L','M','N',
+        'O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b',
+        'c','d','e','f','g','h','i','j','k','l','m','n','o','p',
+        'q','r','s','t','u','v','w','y','z'); # 58
+my (%rb62x, @ascidx, %ascrlist) = ((), (), ());
 
 # @override new
 sub new {
@@ -45,51 +66,460 @@ sub new {
         @args = @{$args};
     }
     my $argc = scalar @args;
-    if($argc > 1){
-        print "args:[$args] length:$argc 0:$class 1:".$args[0]." 2:".$args[1]."\n";
-    }
-    elsif(defined($args)){
-        print "args:[$args] length:$argc\n";
-    }
+
+    $self->_fillRb62x();
+
 	return $self;
 }
 
 #
 # OOP style
-sub encode($ $){
+# encode, ibase=2,8,10,16,32...
+sub encode($ $) {
     my $self = $_[0]; # shift
-    my $rtn = 0;
+    my $output = '';
+    my $input = $_[1];
+    my $ibase = 0;
+    my $argc = scalar @_;
+    if($argc > 2){
+        $ibase = $_[2];
+    }
+    #print $LOGTAG.":encode: argc:[$argc] input1:[$input] 2:[$ibase] input-len:".length($input)."\n";
+    if($input eq ''){ return $output; }
 
-    return $rtn;
+    $codetype = 0;
+    my $rb62xSize = keys %rb62x;
+    if($rb62xSize < 1){ $self->_fillRb62x(); }
+    my $isNum = 0;
+    my $xtag = XTAG;
+    if($ibase > 0){ $isNum = 1; }
+    if($isNum == 1){
+        # number
+        $output = 0;
+        my $num_input = $self->_xx2dec($input, $ibase, \%rb62x);
+        $output = $self->_dec2xx($num_input, $xpos, \@b62x);
+    }
+    else{
+        # string
+        $input = Encode::encode($UTF8, $input); 
+        my @inputArr = split(//, $input); my $inputlen = scalar @inputArr;
+        #print "aft pack:input:[$input] length:".$inputlen."\n"; 
+        my $asctype = $self->_setAscii($codetype, \@inputArr);
+        
+        my @op = (); 
+        $i = 0 ; my $m = 0;
+        if($asctype == 1){
+            # ascii string
+            my $ixtag = ord($xtag);
+            do{
+                $inputArr[$i] = ord($inputArr[$i]);
+                if($ascidx[$inputArr[$i]] ne "-1"){ # has set
+                    $op[$m] = $xtag;
+                    $op[++$m] = $ascidx[$inputArr[$i]];
+                }
+                elsif($inputArr[$i] == $ixtag){
+                    $op[$m] = $xtag;
+                    $op[++$m] = $xtag;
+                }
+                else{
+                    $op[$m] = chr($inputArr[$i]);
+                }
+                $m++;
+            }
+            while(++$i < $inputlen);
+            $op[$m] = $xtag; # asctype=1 has a tag 'x' appended
+        }
+        else{
+            # non-ascii, string
+            my ($c0, $c1, $c2, $c3, $remaini) = (0, 0, 0, 0, 0);
+            do{
+                $remaini = $inputlen - $i;
+                $inputArr[$i] = ord($inputArr[$i]);
+                #print "".($i).": byte:[".$inputArr[$i]."] char:[".chr($inputArr[$i])."]\n";
+                if($remaini == 1){
+                    $c0 = $inputArr[$i] >> 2;
+                    $c1 = (($inputArr[$i] << 6) & 0xff) >> 6;
+                    if($c0 > $bpos){ $op[$m] = $xtag; $op[++$m] = $b62x[$c0]; }
+                    else{ $op[$m] = $b62x[$c0]; } 
+                    if($c1 > $bpos){ $op[++$m] = $xtag; $op[++$m] = $b62x[$c1]; }
+                    else{ $op[++$m] = $b62x[$c1]; } 
+                }
+                elsif($remaini == 2){
+                    $inputArr[$i+1] = ord($inputArr[$i+1]);
+                    $c0 = $inputArr[$i] >> 2;
+                    $c1 = ((($inputArr[$i] << 6) & 0xff) >> 2) | ($inputArr[$i+1] >> 4);
+                    $c2 = (($inputArr[$i+1] << 4) & 0xff) >> 4;
+                    if($c0 > $bpos){ $op[$m] = $xtag; $op[++$m] = $b62x[$c0]; }
+                    else{ $op[$m] = $b62x[$c0]; } 
+                    if($c1 > $bpos){ $op[++$m] = $xtag; $op[++$m] = $b62x[$c1]; }
+                    else{ $op[++$m] = $b62x[$c1]; } 
+                    if($c2 > $bpos){ $op[++$m] = $xtag; $op[++$m] = $b62x[$c2]; }
+                    else{ $op[++$m] = $b62x[$c2]; } 
+                    $i += 1;
+                }
+                else{
+                    $inputArr[$i+1] = ord($inputArr[$i+1]);
+                    $inputArr[$i+2] = ord($inputArr[$i+2]);
+                    $c0 = $inputArr[$i] >> 2;
+                    $c1 = ((($inputArr[$i] << 6) & 0xff) >> 2) | ($inputArr[$i+1] >> 4);
+                    $c2 = ((($inputArr[$i+1] << 4) & 0xff) >> 2) | ($inputArr[$i+2] >> 6);
+                    $c3 = (($inputArr[$i+2] << 2) & 0xff) >> 2;
+                    if($c0 > $bpos){ $op[$m] = $xtag; $op[++$m] = $b62x[$c0]; }
+                    else{ $op[$m] = $b62x[$c0]; } 
+                    if($c1 > $bpos){ $op[++$m] = $xtag; $op[++$m] = $b62x[$c1]; }
+                    else{ $op[++$m] = $b62x[$c1]; } 
+                    if($c2 > $bpos){ $op[++$m] = $xtag; $op[++$m] = $b62x[$c2]; }
+                    else{ $op[++$m] = $b62x[$c2]; } 
+                    if($c3 > $bpos){ $op[++$m] = $xtag; $op[++$m] = $b62x[$c3]; }
+                    else{ $op[++$m] = $b62x[$c3]; } 
+                    $i += 2;
+                }
+                $m++; 
+            }
+            while(++$i < $inputlen);
+        }
+        $output = join('', @op);
+    }
+    return $output;
 }
 
 #
-sub decode($){
+# decode, obase=2,8,10,16,32... 
+sub decode($ $){
     my $self = $_[0]; # shift
-    my $rtn = 0;
+    my $output = '';
+    my $input = $_[1];
+    my $ibase = 0;
+    my $argc = scalar @_;
+    if($argc > 2){
+        $ibase = $_[2];
+    }
+    if($input eq ''){ return $output; }
 
+    $codetype = 1;
+    my $xtag = XTAG;
+    my $rb62xSize = keys %rb62x;
+    if($rb62xSize < 1){ $self-> _fillRb62x(); }
+    my $isNum = 0;
+    if($ibase > 0){ $isNum = 1; }
+    if($isNum == 1){
+        # number
+        $output = 0;
+        my $num_input = $self->_xx2dec($input, $xpos, \%rb62x);
+        $output = $self->_dec2xx($num_input, $ibase, \@b62x);
+    }
+    else{
+        # string
+        my @inputArr = split(//, $input); my $inputlen = scalar @inputArr;
+        #print "aft pack:input:[$input] length:".$inputlen." codetype:[$codetype]\n"; 
+        my $asctype = $self->_setAscii($codetype, \@inputArr);
+        
+        my @op = (); 
+        $i = 0 ; my $m = 0;
+        if($asctype == 1){
+            # ascii
+            $inputlen--; # omit last 'x'
+            do{
+                if($inputArr[$i] eq $xtag){
+                    if($inputArr[$i+1] eq $xtag){
+                        $op[$m] = $xtag;
+                        $i++;
+                    }
+                    else{
+                        $op[$m] = chr($ascrlist{$inputArr[++$i]});
+                    }
+                }
+                else{
+                    $op[$m] = $inputArr[$i];
+                }
+                $m++;
+            }
+            while(++$i < $inputlen);
+        }
+        else{
+            # non-ascii
+            my (@tmpArr, @arr, $arr) = ((), (), undef); my $remaini = 0;
+            my @bint = (0, 1, 2, 3);
+            do{
+                @tmpArr = (undef, undef, undef, undef);
+                $remaini = $inputlen - $i;
+                #print "".($i).": byte:[".$inputArr[$i]."] char:[".ord($inputArr[$i])."]\n";
+                if($remaini == 1){
+                    print($LOGTAG.": found illegal base62x input:[".$inputArr[$i]."]. 1608091042.");
+                    next;
+                }
+                elsif($remaini == 2){
+                    if($inputArr[$i] eq $xtag){ $tmpArr[0] = $bpos + $bint[$inputArr[++$i]]; }
+                    else{$tmpArr[0] = $rb62x{$inputArr[$i]}; }
+                    if($inputArr[++$i] eq $xtag){ $tmpArr[1] = $bpos + $bint[$inputArr[++$i]]; }
+                    else{$tmpArr[1] = $rb62x{$inputArr[$i]}; }
+                    $arr = $self->_decodeByLength(\@tmpArr, \@op, $m);
+                    @arr = @{$arr}; @op = @{$arr[0]}; $m = $arr[1];
+                }
+                elsif($remaini == 3){
+                    if($inputArr[$i] eq $xtag){ $tmpArr[0] = $bpos + $bint[$inputArr[++$i]]; }
+                    else{$tmpArr[0] = $rb62x{$inputArr[$i]}; }
+                    if($inputArr[++$i] eq $xtag){ $tmpArr[1] = $bpos + $bint[$inputArr[++$i]]; }
+                    else{$tmpArr[1] = $rb62x{$inputArr[$i]}; }
+                    if($inputArr[++$i] eq $xtag){ $tmpArr[2] = $bpos + $bint[$inputArr[++$i]]; }
+                    else{$tmpArr[2] = $rb62x{$inputArr[$i]}; }
+                    $arr = $self->_decodeByLength(\@tmpArr, \@op, $m);
+                    @arr = @{$arr}; @op = @{$arr[0]}; $m = $arr[1];
+                }
+                else{
+                    if($inputArr[$i] eq $xtag){ $tmpArr[0] = $bpos + $bint[$inputArr[++$i]]; }
+                    else{$tmpArr[0] = $rb62x{$inputArr[$i]}; }
+                    if($inputArr[++$i] eq $xtag){ $tmpArr[1] = $bpos + $bint[$inputArr[++$i]]; }
+                    else{$tmpArr[1] = $rb62x{$inputArr[$i]}; }
+                    if($inputArr[++$i] eq $xtag){ $tmpArr[2] = $bpos + $bint[$inputArr[++$i]]; }
+                    else{$tmpArr[2] = $rb62x{$inputArr[$i]}; }
+                    if($inputArr[++$i] eq $xtag){ $tmpArr[3] = $bpos + $bint[$inputArr[++$i]]; }
+                    else{$tmpArr[3] = $rb62x{$inputArr[$i]}; }
+                    $arr = $self->_decodeByLength(\@tmpArr, \@op, $m);
+                    @arr = @{$arr}; @op = @{$arr[0]}; $m = $arr[1];
+                }
+                $m++;
+            }
+            while(++$i < $inputlen);
+        }
+        $output = join('', @op);
+    }
+    $output = Encode::decode($UTF8, $output); 
+    return $output;
+}
+
+#
+# functional style, static
+sub base62x_encode {
+    #my $self = $_[0];
+    my $self = Base62x->new();
+    my $rtn = '';
+    my $input = $_[0];
+    my $ibase = 0;
+    my $argc = scalar @_;
+    if($argc > 1){
+        $ibase = $_[1];
+    }
+    $rtn = $self->encode($input, $ibase);
     return $rtn;
 }
 
 #
-# functional style
-sub encode_base62x($ $){
-    my $rtn = 0;
-
+sub base62x_decode {
+    my $self = Base62x->new();
+    my $rtn = '';
+    my $input = $_[0];
+    my $obase = 0;
+    my $argc = scalar @_;
+    if($argc > 1){
+        $obase = $_[1];
+    }
+    $rtn = $self->decode($input, $obase);
     return $rtn;
 }
 
 #
-sub decode_base62x($){
-    my $rtn = 0;
+sub _fillRb62x(){
+    my $self = $_[0];
+    for($i=0; $i<=$xpos; $i++){
+        if($i > $bpos && $i < $xpos){
+            # omit x1, x2, x3
+        } 
+        else{
+            $rb62x{$b62x[$i]} = $i;
+        }
+    }
+    return 0;
+}
 
+# 
+# inner methods 
+#
+sub _setAscii($ $){
+    my $self = $_[0];
+    my $asctype = 0;
+    my $codetype = $_[1];
+    my @inputArr = @{$_[2]};
+    my $inputlen = scalar @inputArr;
+    if($codetype == 0 && ord($inputArr[0]) <= $ascmax){
+        $asctype = 1; my $tmpi = 0;
+        for($i=1; $i<$inputlen; $i++){
+            $tmpi = ord($inputArr[$i]);
+            if($tmpi > $ascmax
+                || ($tmpi > 16 && $tmpi < 21) # DC1-4
+                || ($tmpi > 27 && $tmpi < 32)){ # FC, GS, RS, US
+                $asctype = 0;
+                last;
+            }
+        }
+    }
+    elsif($codetype == 1 && $inputArr[$inputlen - 1] eq XTAG){
+        $asctype = 1;       
+    }
+    if($asctype == 1){
+        my $idxlen = scalar @ascidx;
+        if($idxlen < 1){
+            $self->_fillAscRlist();    
+        }
+    }
+    return $asctype;
+}
+
+#
+sub _fillAscRlist(){
+    my $self = $_[0];
+    for($i=0; $i<$ascmax; $i++){ $ascidx[$i] = -1; }
+    my $idxi = 0;
+    my @bgnArr = (0, 21, 32, 58, 91, 123);
+    my @endArr = (17, 28, 48, 65, 97, $ascmax+1);
+    my $arrSize = scalar @bgnArr;
+    for(my $ai=0; $ai<$arrSize; $ai++){
+        my $bgn = $bgnArr[$ai];
+        my $end = $endArr[$ai];
+        for($i=$bgn; $i<$end; $i++){
+            $ascidx[$i] = $asclist[$idxi];
+            $ascrlist{$asclist[$idxi]} = $i;
+            $idxi++;
+        }
+    }
+    return 0;
+}
+
+#
+sub _decodeByLength($ $ $){
+    my $self = $_[0];
+    my @rtnArr = ();
+    my @tmpArr = @{$_[1]};
+    my @op = @{$_[2]};
+    my $m = $_[3];
+    my ($c0, $c1, $c2) = (0, 0, 0);
+    if(defined($tmpArr[3])){
+        $c0 = $tmpArr[0] << 2 | $tmpArr[1] >> 4;
+        $c1 = (($tmpArr[1] << 4) & 0xf0) | ($tmpArr[2] >> 2);
+        $c2 = (($tmpArr[2] << 6) & 0xff) | $tmpArr[3];
+        $op[$m] = chr($c0);
+        $op[++$m] = chr($c1);
+        $op[++$m] = chr($c2);
+    }
+    elsif(defined($tmpArr[2])){
+        $c0 = $tmpArr[0] << 2 | $tmpArr[1] >> 4;
+        $c1 = (($tmpArr[1] << 4) & 0xf0) | $tmpArr[2];
+        $op[$m] = chr($c0);
+        $op[++$m] = chr($c1);
+    }
+    elsif(defined($tmpArr[1])){
+        $c0 = $tmpArr[0] << 2 | $tmpArr[1];
+        $op[$m] = chr($c0);
+    }
+    else{
+        $c0 = $tmpArr[0];
+        $op[$m] = chr($c0);
+    }
+    $rtnArr[0] = \@op;
+    $rtnArr[1] = $m;
+    return \@rtnArr;
+}
+
+#
+# xx2dec, ibase=2, 3, 4, 5...62
+sub _xx2dec($ $ $){
+    my $self = $_[0];
+    my $rtn = 0;
+    my $inum = $_[1];
+    my $ibase = $_[2];
+    my %ridx = %{$_[3]}; 
+    my $xtag = XTAG;
+    if($ibase <= $max_safe_base){
+        $rtn = $self->_baseFrom($inum, $ibase);
+    }
+    else{
+        my @inArr = split(//, $inum);
+        @inArr = reverse(@inArr);
+        my $arrSize = scalar @inArr;
+        my $xnum = 0; my $tmpi = 0;
+        for($i=0; $i<$arrSize; $i++){
+            if(defined($inArr[$i+1]) && $inArr[$i+1] eq $xtag){
+                $tmpi = $bpos + $ridx{$inArr[$i]}; 
+                $xnum++;
+                $i++;
+            }
+            else{
+                $tmpi = $ridx{$inArr[$i]};
+            }
+            if($tmpi >= $ibase){
+                print "".$LOGTAG.": _xx2dec found out of radix:$tmpi for base:$ibase.\n";
+                $tmpi = $ibase - 1;
+            }
+            $rtn = $rtn + $tmpi * ($ibase ** ($i - $xnum));
+        }
+    }
+    #print "".$LOGTAG.": _xx2dec: innum:$inum ibase:$ibase rtn:$rtn\n";
     return $rtn;
 }
 
 #
-sub _privateMethod(){
+# dec2xx, obase=2, 3, 4, 5...62
+sub _dec2xx($ $ $){
+    my $self = $_[0];
     my $rtn = 0;
+    my $inum = $_[1];
+    my $obase = $_[2];
+    my @ridx = @{$_[3]}; 
+    my $xtag = XTAG;
+    if($obase <= $max_safe_base){
+        $rtn = $self->_baseTo($inum, $obase);
+    }
+    else{
+        $i = 0;
+        my $b = 0; my @outArr = ();
+        do{
+            $b = $inum % $obase;
+            $inum = int($inum / $obase);
+            if($b <= $bpos){
+                $outArr[$i++] = $ridx[$b];
+            }
+            else{
+                $outArr[$i++] = $ridx[$b - $bpos];
+                $outArr[$i++] = $xtag;
+            }
+        }
+        while($inum >= $obase);
+        $b = $inum;
+        if($b > 0){
+            if($b <= $bpos){
+                $outArr[$i++] = $ridx[$b];
+            }
+            else{
+                $outArr[$i++] = $ridx[$b - $bpos];
+                $outArr[$i++] = $xtag;
+            }
+        }
+        @outArr = reverse(@outArr);
+        $rtn = join('', @outArr);
+    }
+    #print "".$LOGTAG.": _dec2xx: innum:".$_[1]." obase:$obase rtn:$rtn\n";
     return $rtn;
+}
+
+#
+sub _baseTo($ $){
+    my($n,$b) = ($_[1], $_[2]);
+    my $s = "";
+    while ($n) {
+        $s .= ('0'..'9','A'..'Z')[$n % $b];
+        $n = int($n/$b);
+    }
+    return scalar(reverse($s));
+}
+
+#
+sub _baseFrom($ $){
+    my($n,$b) = ($_[1], $_[2]);
+    my $t = 0;
+    for my $c (split(//, uc($n))) {
+        $t = $b * $t + index("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ", $c);
+    }
+    return $t;
 }
 
 1;
--- base62xtest.pl
+++ base62xtest.pl
@@ -15,8 +15,8 @@ use File::Basename qw(dirname basename);
 use Time::HiRes qw(sleep time);
 use Fcntl qw(:flock);
 use POSIX qw(strftime);
-use Encode qw(decode_utf8 encode_utf8);
-use autodie;
+#use Encode qw(decode_utf8 encode_utf8);
+#use autodie;
 
 use utf8;
 no warnings 'utf8';
@@ -25,7 +25,8 @@ binmode( STDOUT, ':encoding(utf8)' );
 binmode( STDERR, ':encoding(utf8)' );
 
 #use MIME::Base62x;
-use Base62x;
+#use Base62x;
+use Base62x qw(base62x_encode base62x_decode);
 
 my $mydir = dirname(abs_path($0));
 my $basename = basename($0,(".pl"));
@@ -33,4 +34,40 @@ my $basename = basename($0,(".pl"));
 my $base62x = Base62x->new(); # \@ARGV 
 
 print "workdir:$mydir self:$basename base62x:$base62x\n";
+
+my $s = "中abc文123-"; # "This is a page for -Base62x. / 这里是 -Base62x . @2018-09-03-10:44:37+0100";
+my $enc = $base62x->encode($s);
+$enc = "L6XfSo1fSo1X871XPsKWPcx1o82r2ONDbDZ9uBY0l8EYx3cUc7ZEQOho0jGc5pPJOoU20k840oC34uBJ0vBJ0qBJ0uEZKtEZKuAp0nC30A2aXlRcLpT7aWOMva86HfR6bdPMvZPI1pQ6x1rR6GWOcKWUMx1rSY1bT6LoRc5i86rXT6LpBY0jBKPoOMvhR6bk82zWwAx2QvQwUvBYEvOkavOk9vRgKwAx2bveYGvBYwvBsWvh2uvBc5vvg4vBoqvBwZuu228EA0bEA0bEMlZEM5iEM5Yx2QUb3";
+my $dec = $base62x->decode($enc);
+my $dec2 = base62x_decode($enc);
+print "orig-s:[$s]\n\tenc:[$enc]\n\tdec:[$dec]\n\tdec2:[$dec2]\n---- ---- ----\n";
+
+$s = "onclick=\"javascript:parent.成功移植</a></h4></div>";
+$enc = $base62x->encode($s);
+my $enc2 = base62x_encode($s);
+$dec = $base62x->decode($enc2);
+print "orig-s:[$s]\n\tenc:[$enc]\n\tenc2:[$enc2]\n\tdec:[$dec]\n---- ---- ----\n";
+
+$s = "onclick=\"javascript:parent.</a></h4></div>";
+$enc = $base62x->encode($s);
+$dec = $base62x->decode($enc);
+print "orig-s:[$s]\n\tenc:[$enc]\n\tdec:[$dec]\n---- ---- ----\n";
+
+$s = "[2016-09-12 18:24:56] Stas || OfferSeven: conversions";
+$enc = $base62x->encode($s);
+$dec = $base62x->decode($enc);
+print "orig-s:[$s]\n\tenc:[$enc]\n\tdec:[$dec]\n---- ---- ----\n";
+
+$s = "Добро пожаловать в Википедию,";
+$enc = $base62x->encode($s);
+$dec = $base62x->decode($enc);
+print "orig-s:[$s]\n\tenc:[$enc]\n\tdec:[$dec]\n---- ---- ----\n";
+
+$s = "abc 1F 10 100 FF11 10000 F212 F1D2 F1d2"; my $xbase=16;
+my @arr = split(/ /, $s);
+foreach $s (@arr){
+    $enc = $base62x->encode($s, $xbase);
+    $dec = $base62x->decode($enc, $xbase);
+    print "orig-s:[$s]\n\tenc:[$enc]\n\tdec:[$dec]\n---- ---- ----\n";
+}
 
